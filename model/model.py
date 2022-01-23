b9a95c9c31f92f7b1ddf70cb02023ceba66804c0
Tom Andersen <tomaandersen96@gmail.com>
2022-01-23 12:56:00 +0100

    added requirements

--- model/model.py
+++ model/model.py
@@ -1,10 +1,24 @@
+"""
+Maze world interface class
+"""
+
+
 class IMaze:
 
     def is_traversable(self, x, y) -> bool:
+        """
+        :return: True if point (x, y) is traversable.
+        """
         raise NotImplementedError()
 
     def get_width(self) -> int:
+        """
+        :return: Returns the width of the map.
+        """
         raise NotImplementedError()
 
     def get_height(self) -> int:
+        """
+        :return: Returns the height of the map.
+        """
         raise NotImplementedError()
