12e738a36fdbd34a66d17aabcaf1a11efcb876eb
Tom Andersen <tomaandersen96@gmail.com>
2022-01-23 12:45:38 +0100

    added model interface

---
+++ model/model.py
@@ -0,0 +1,10 @@
+class IMaze:
+
+    def is_traversable(self, x, y) -> bool:
+        raise NotImplementedError()
+
+    def get_width(self) -> int:
+        raise NotImplementedError()
+
+    def get_height(self) -> int:
+        raise NotImplementedError()
